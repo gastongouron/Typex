97e71fb61076ae48e6d5ec6da67bf9536e0aadcf
gastongouron <gastongourogmail.l.com>
2015-12-13 17:15:59 -0800

    Update from JS to jQuery to listen to events

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
- WebFontConfig = {
+// Google fonts call. line 2 to 13 provided by google
+WebFontConfig = {
     google: { families: [ 'Open+Sans:400,300,300italic,400italic,600,800italic,800,700italic,700,600italic:latin,latin-ext' ] }
-  };
-  (function() {
+};
+(function() {
     var wf = document.createElement('script');
     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
     wf.type = 'text/javascript';
     wf.async = 'true';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
-  })();
+})();
 
+
+// Create a variable equal to the actual div where id="output"
+// And an hash to store pressed keys and their duration
 var output = document.getElementById('output'),
-    pressed = {};
+    pressed = 0
+var character
+// Listen to keydown via window element
+$("body").on("keypress", function(e) {
+   if (pressed) return;
+   character = String.fromCharCode(e.which);
+   pressed=e.timeStamp;
+});
 
-window.onkeydown = function(e) {
-    if ( pressed[e.which] ) return;
-    pressed[e.which] = e.timeStamp;
-};
+// Listen to keyup via window element
+$("body").on("keyup", function(e) {
+    if ( !pressed ) return;
 
-window.onkeyup = function(e) {
-    if ( !pressed[e.which] ) return;
-        var duration = ( e.timeStamp - pressed[e.which] ) / 1000;
-        var letter = String.fromCharCode(e.which);
-        var lowerCaseLetter = letter.toLowerCase();
+        // get duration of the key pressed event
+        var duration = ( e.timeStamp - pressed ) / 1000;
+        // Output the letter in a span tag
+        output.innerHTML += '<span class="klass">' + character + '</span>';
 
-        output.innerHTML += '<span class="klass">' + lowerCaseLetter + '</span>';
-
+        // Get the next one with classname index -1
         var target_array = document.getElementsByClassName('klass');
         var target = target_array[target_array.length - 1];
 
+        // Change character family to Open Sans and font style to Italic
         target.style.fontFamily = "open sans";
         target.style.fontStyle = "italic";
 
+    // Conditional statement that modify fontWeight referring to the duration of the key pressed event
     if (duration < 0.04)
         target.style.fontWeight = 300;
     else if (duration < 0.1)
@@ -44,5 +54,6 @@ window.onkeyup = function(e) {
     else
         target.style.fontWeight = 800;
 
-        pressed[e.which] = 0;
-};
+        // Reinitialize pressed to 0
+        pressed = 0;
+});
